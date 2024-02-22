5bb274abc36e9f2bbdbac3d5ed17a73d35fa837d
Artur Agh <aghartur@gmail.com>
2024-02-22 15:03:27 +0100

    dev branch login commit 1

--- src/user/User.tsx
+++ src/user/User.tsx
@@ -6,6 +6,9 @@ export const User = () => {
       User-344331
       <h1>Dev branch commit 1</h1>
       <p>Dev branch commit 1</p>
+      <p>Dev branch commit 1</p>
+      <p>Dev branch commit 1</p>
+      <p>Develop/login branch commit 1</p>
     </div>
   );
 };
