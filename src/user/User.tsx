8d529202e365f1cd31e9bc690cb3470531504699
Artur Agh <aghartur@gmail.com>
2024-02-22 15:06:37 +0100

    popup commit

--- src/user/User.tsx
+++ src/user/User.tsx
@@ -6,6 +6,8 @@ export const User = () => {
       User-344331
       <h1>Dev branch commit 1</h1>
       <p>Dev branch commit 1</p>
+      <p>Popup branch commit 1</p>
+      <p>Popup branch commit 1</p>
     </div>
   );
 };
