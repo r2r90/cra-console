293404e24f1ae0d5aab7fba8e7ca776c356f8317
Artur Agh <aghartur@gmail.com>
2024-02-22 14:49:21 +0100

    dev branch commit

--- src/user/User.tsx
+++ src/user/User.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export const User = () => {
-    return (
-        <div>User-344331
-            <p>Lorem ipsum dolor.</p>
-
-        </div>
-
-    )
-}
+  return (
+    <div>
+      User-344331
+      <h1>Dev branch commit 1</h1>
+    </div>
+  );
+};
