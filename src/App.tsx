4e435b29abcbd19fc00e0a6fa183464d0c0b6301
Artur Agh <aghartur@gmail.com>
2024-02-22 11:58:44 +0100

    Commit - 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,28 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>Lorem ipsum dolor.</p>
-        <p>Lorem ipsum dolor.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-               Learn React
-        </a>
-<p>11</p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <p>Lorem ipsum dolor.</p>
+                <p>Lorem ipsum dolor.</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <p>11</p>
+                <p>11</p>
+                <p>11</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
