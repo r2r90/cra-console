2c14357c4b82040713df328fa8d31da9ea22eef9
Artur Agh <aghartur@gmail.com>
2024-02-22 15:35:57 +0100

    changes App- test

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <p>Lorem ipsum dolor.</p>
-                <p>Lorem ipsum dolor.</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <p>11</p>
-                <p>11</p>
-                <p>11</p>
-                <p>11</p>
-                <p>11</p>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>Lorem ipsum dolor.</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
